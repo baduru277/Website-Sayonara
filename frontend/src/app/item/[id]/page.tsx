f2b1fac89f4991f28fc4c676914fd8fb8e32a7b7
Bindu Sarika Aduru <baduru@justransform.com>
2025-07-05 02:50:02 +0530

    update

--- frontend/src/app/item/[id]/page.tsx
+++ frontend/src/app/item/[id]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { useParams } from 'next/navigation';
 import Header from '../../../components/Header';
 import Footer from '../../../components/Footer';
 
