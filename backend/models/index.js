ca4387ef966c932d6f020ad1493ba0871c96a81f
baduru277 <baduru@justransform.com>
2025-10-16 23:24:17 +0530

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,12 +3,13 @@ const Item = require('./Item');
 const Bid = require('./Bid');
 
 User.hasMany(Item, { foreignKey: 'userId', as: 'items' });
-Item.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+Item.belongsTo(User, { foreignKey: 'userId', as: 'seller' });
 
 User.hasMany(Bid, { foreignKey: 'userId', as: 'bids' });
-Bid.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+Bid.belongsTo(User, { foreignKey: 'userId', as: 'bidder' });
 
 Item.hasMany(Bid, { foreignKey: 'itemId', as: 'bids' });
 Bid.belongsTo(Item, { foreignKey: 'itemId', as: 'item' });
+;
 
 module.exports = { User, Item, Bid };
