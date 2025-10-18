e39a6e06f8e6c1afb42ad2184d90554aa6f697e2
baduru277 <baduru@justransform.com>
2025-10-18 16:05:50 +0530

    Add sequelize to models for associations

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,10 +1,12 @@
+const sequelize = require('../config/database');
 const User = require('./User');
 const Item = require('./Item');
 const Bid = require('./Bid');
 
-// Call associate functions to set up relationships
-const models = { User, Item, Bid };
+// Define associations
+const models = { User, Item, Bid, sequelize };
 
+// Call associate functions to set up relationships
 Object.values(models).forEach(model => {
   if (model.associate) {
     model.associate(models);
