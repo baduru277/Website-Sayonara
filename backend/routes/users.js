f1594f634f4c2305e0f84a91e881587aaf7fc37a
baduru277 <baduru@justransform.com>
2025-10-26 22:43:40 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,7 +5,7 @@ const { auth } = require('../middleware/auth');
 const router = express.Router();
 
 // Get user profile
-router.get('/profile', auth, async (req, res) => {
+router.get('/dashboard', auth, async (req, res) => {
   try {
     const user = await User.findByPk(req.user.id, {
       attributes: { exclude: ['password'] }
