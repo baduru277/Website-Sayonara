674e45a477202e9e02b7bdfc997113131f8f115c
baduru277 <baduru@justransform.com>
2025-10-28 19:03:22 +0530

    Update page.tsx

--- frontend/src/app/add-item/page.tsx
+++ frontend/src/app/add-item/page.tsx
@@ -1,5 +1,8 @@
 'use client';
-
+console.log('ENV:', {
+  API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NODE_ENV: process.env.NODE_ENV
+});
 import { useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
