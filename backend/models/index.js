eb8de6e6628a4a61f452e75b9474fe48fbc1dae3
baduru277 <baduru@justransform.com>
2026-03-01 23:56:39 +0530

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -4,9 +4,11 @@ const Item = require('./Item');
 const Bid = require('./Bid');
 const Subscription = require('./Subscription');
 const PaymentProof = require('./PaymentProof'); // ✅ ADD THIS
+const messagesRouter = require('./messages');
+
 
 // Define all models
-const models = { User, Item, Bid, Subscription, PaymentProof, sequelize }; // ✅ ADD PaymentProof
+const models = { User, Item, Bid, Subscription, PaymentProof, sequelize,messages }; // ✅ ADD PaymentProof
 
 // Call associate functions to set up all relationships
 Object.values(models).forEach(model => {
